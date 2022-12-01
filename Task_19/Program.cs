554a850c2434eb0c3988d5b4a5f0a1e396a508b9
Nikita <sorokovsky.ni@yandex.ru>
2022-12-01 21:18:23 +0300

    Done Homework

---
+++ Task_19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Task19
+Console.Clear();
+Console.WriteLine("Enter the number: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Your number: {number} - palindrome.");
+  }
+  else Console.WriteLine($"Your number: {number} - It's not a palindrome .");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"NO");
